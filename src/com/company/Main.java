bc2f7a179f75e56b6d43fde918d2a77e0b9d7a41
castro <c9stros@gmail.com>
2022-01-26 19:56:36 -0500

    the red car is ready for trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         //the basic structure
         CarBasics car=new CarBasics();
+        car.color="red";
+        car.doors="2";
+        car.engine="7999cc";
+        car.number="229018";
+        System.out.println(car.color);
 
     }
 }
